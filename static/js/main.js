111284236a2adb2ec0752c2851a6b3ce7ec32029
shinying <shinying@mac>
2024-03-28 21:15:37 +0800

    fix image loading

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(window).on('load', function(){
   var options = {
     slidesToScroll: 1,
     slidesToShow: 3,
@@ -7,7 +7,7 @@ $(function(){
     autoplay: false,
     autoplaySpeed: 3000,
   }
-  const carousel_3d_photo = bulmaCarousel.attach('#carousel-3d-photo', options);
+  bulmaCarousel.attach('#carousel-3d-photo', options);
 
   var options_demo = {
     slidesToScroll: 1,
@@ -16,7 +16,7 @@ $(function(){
     infinite: false,
     autoplay: false,
   }
-  const carousel_demo = bulmaCarousel.attach('.demo-carousel', options_demo);
+  bulmaCarousel.attach('.demo-carousel', options_demo);
 
   $(".image-compare").twentytwenty({
     default_offset_pct:	0.7,
